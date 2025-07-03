5f6f805ba5fd48224cea97ebbae5d8c323603bfc
hi1arina <158295757+hi1arina@users.noreply.github.com>
2025-07-03 13:19:04 +0300

    Если success=true, то reason пуст. Поправлен этот момент в data классах

--- app/src/main/java/com/example/leasing/cars_catalog/data/model/CarsResponseModel.kt
+++ app/src/main/java/com/example/leasing/cars_catalog/data/model/CarsResponseModel.kt
@@ -2,7 +2,7 @@ package com.example.leasing.cars_catalog.data.model
 
 data class CarsResponseModel(
     val success: Boolean,
-    val reason: String,
+    val reason: String?,
     val data: List<CarModel>,
     val meta: MetaModel
 )
--- app/src/main/java/com/example/leasing/cars_catalog/domain/entity/CarsResponse.kt
+++ app/src/main/java/com/example/leasing/cars_catalog/domain/entity/CarsResponse.kt
@@ -3,7 +3,7 @@ package com.example.leasing.cars_catalog.domain.entity
 
 data class CarsResponse(
     val success: Boolean,
-    val reason: String,
+    val reason: String?,
     val data: List<Car>,
     val meta: Meta
 )
