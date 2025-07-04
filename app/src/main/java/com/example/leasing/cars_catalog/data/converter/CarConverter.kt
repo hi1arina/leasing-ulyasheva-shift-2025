e19ecb422ca348ab23edc4405a28253720dc6546
hi1arina <158295757+hi1arina@users.noreply.github.com>
2025-07-04 14:36:26 +0300

    Так как значения, приходящие с сервера, находятся в lowercase, а в коде enum классы были изменены на uppercase, то 1) типы полей в CarModel были изменены с enum классов на String 2) для каждого enum сделаны мапперы

--- app/src/main/java/com/example/leasing/cars_catalog/data/converter/CarConverter.kt
+++ app/src/main/java/com/example/leasing/cars_catalog/data/converter/CarConverter.kt
@@ -1,36 +1,68 @@
 package com.example.leasing.cars_catalog.data.converter
 
 import com.example.leasing.cars_catalog.data.model.CarModel
+import com.example.leasing.cars_catalog.domain.entity.BodyType
 import com.example.leasing.cars_catalog.domain.entity.Brand
 import com.example.leasing.cars_catalog.domain.entity.Car
+import com.example.leasing.cars_catalog.domain.entity.Color
+import com.example.leasing.cars_catalog.domain.entity.Steering
+import com.example.leasing.cars_catalog.domain.entity.Transmission
 
 object CarConverter {
 
     private fun mapBrand(brand: String): Brand = when (brand.trim()) {
-        "Haval" -> Brand.Haval
-        "Hyundai" -> Brand.Hyundai
-        "Volkswagen" -> Brand.Volkswagen
-        "Kia" -> Brand.Kia
-        "Geely" -> Brand.Geely
-        "Mercedes" -> Brand.Mercedes
-        "Garden car" -> Brand.GardenCar
-        "Grocery cart" -> Brand.GroceryCart
-        "Haier" -> Brand.Haier
-        else -> Brand.Invalid
+        "Haval" -> Brand.HAVAL
+        "Hyundai" -> Brand.HYUNDAI
+        "Volkswagen" -> Brand.VOLKSWAGEN
+        "Kia" -> Brand.KIA
+        "Geely" -> Brand.GEELY
+        "Mercedes" -> Brand.MERCEDES
+        "Garden car" -> Brand.GARDENCAR
+        "Grocery cart" -> Brand.GROCERYCART
+        "Haier" -> Brand.HAIER
+        else -> Brand.INVALID
     }
 
-    fun convert(car: CarModel) : Car =
-        Car (
+    private fun mapTransmission(transmission: String): Transmission = when (transmission.trim()) {
+        "automatic" -> Transmission.AUTOMATIC
+        else -> Transmission.MANUAL
+    }
+
+    private fun mapBodyType(bodyType: String): BodyType = when (bodyType.trim()) {
+        "sedan" -> BodyType.SEDAN
+        "suv" -> BodyType.SUV
+        "coupe" -> BodyType.COUPE
+        "hatchback" -> BodyType.HATCHBACK
+        else -> BodyType.CABRIOLET
+    }
+
+    private fun mapSteering(steering: String): Steering = when (steering.trim()) {
+        "left" -> Steering.LEFT
+        else -> Steering.RIGHT
+    }
+
+    private fun mapColor(color: String): Color = when (color.trim()) {
+        "black" -> Color.BLACK
+        "white" -> Color.WHITE
+        "red" -> Color.RED
+        "silver" -> Color.SILVER
+        "blue" -> Color.BLUE
+        "grey" -> Color.GREY
+        else -> Color.ORANGE
+    }
+
+    fun convert(car: CarModel): Car =
+        Car(
             id = car.id,
             name = car.name,
             brand = mapBrand(car.brand),
             media = car.media.map { MediaConverter.convert(it) },
-            transmission = car.transmission,
+            transmission = mapTransmission(car.transmission),
             price = car.price,
             location = car.location,
-            color = car.color,
-            bodyType = car.bodyType,
-            steering = car.steering
+            color = mapColor(car.color),
+            bodyType = mapBodyType(car.bodyType),
+            steering = mapSteering(car.steering)
         )
 
 }
