c1f891ac2f7d2981d5994474d7ae220c7dc571a8
hi1arina <158295757+hi1arina@users.noreply.github.com>
2025-07-05 14:00:44 +0300

    Подключены библиотеки для работы с ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,6 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
     implementation(libs.okhttp.logging)
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.androidx.runtime.livedata)
 }
